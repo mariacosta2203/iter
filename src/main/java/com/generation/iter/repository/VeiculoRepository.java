8cd50c5200caca526cfd8a2b014c6a5ea343b7fc
beasb <bia4sb4@gmail.com>
2025-01-28 11:56:49 -0300

    CRUD veiculo

--- src/main/java/com/generation/iter/repository/VeiculoRepository.java
+++ src/main/java/com/generation/iter/repository/VeiculoRepository.java
@@ -1,5 +1,13 @@
 package com.generation.iter.repository;
 
-public interface VeiculoRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.iter.model.VeiculoModel;
+
+public interface VeiculoRepository extends JpaRepository<VeiculoModel, Long>{
+
+	public List<VeiculoModel> findAllByModeloContainingIgnoreCase(@Param("modelo") String modelo);
 }
